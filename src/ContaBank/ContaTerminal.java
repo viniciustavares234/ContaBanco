5cbdf6ab7d0f1b31cde279d409667eb6c7a81b05
vinic <viniciustavares234@gmail.com>
2024-09-16 20:36:15 -0300

    initial commit

--- src/ContaBank/ContaTerminal.java
+++ src/ContaBank/ContaTerminal.java
@@ -1,7 +1,35 @@
 package ContaBank;
 
-public class ContaTerminal {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception  {
+        Scanner scanner = new Scanner(System.in);
+
+        // Variáveis de conta e cliente
+        int numeroConta;
+        String agencia ;
+        String nomeCliente;
+        double saldo ;
+
+        System.out.println("Por favor, digite o número da conta:");
+        numeroConta = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.println("Por favor digite o numero da sua Agencia");
+        agencia = scanner.nextLine();
+
+        System.out.println("Por favor digite seu nome: ");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor digite o seu saldo: ");
+        saldo = scanner.nextDouble();
+
+
+        scanner.close();
+
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + String.format("%.2f", saldo) + " já está disponível para saque";
+        System.out.println(mensagem);
     }
 }
